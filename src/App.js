0444c4c79a7390380c2783e5ee6b66ac0b867250
Padmakar <PadmakarPatil@gmail.com>
2024-03-01 04:15:03 +0530

    second Commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Login } from './Login/Login';
 import { Pages } from './Pages/Pages';
 import { Navigate, Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { Button } from '@mui/material'
 function App() {
   return (
+  <div>
+    <Button variant="outlined">Outlined</Button>
    <Routes>
     <Route path="/" element={<Navigate to="Login"/>}></Route>
     <Route path="/login" element={<Login></Login>}></Route>
     <Route path="/pages" element={<Pages></Pages>}></Route>
    </Routes>
+   </div>
   );
 }
 
