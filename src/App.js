e90f52e1c49296a1bd86c30688edddf7e8f9fe68
Padmakar <PadmakarPatil@gmail.com>
2024-03-02 13:42:40 +0530

    Login page added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Login } from './Login/Login';
 import { Pages } from './Pages/Pages';
 import { Navigate, Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import { Button } from '@mui/material'
+
 function App() {
   return (
   <div>
-    <Button variant="outlined">Outlined</Button>
+   
    <Routes>
     <Route path="/" element={<Navigate to="Login"/>}></Route>
     <Route path="/login" element={<Login></Login>}></Route>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Card,CardActions,Button,CardContent , CardHeader, TextField} from "@mui/material";
+import "./Login.css"
 export function Login (){
     return(
-    <div>Login Page</div>
+        <div className="Login-container">
+            <Card sx={{ Width: 400, padding: 3}}>
+            <CardHeader title="Sign In"></CardHeader>
+        <CardContent>
+        <TextField fullWidth label="Email" variant="outlined" margin="normal" />
+        <TextField fullWidth label="Password" variant="outlined" margin="normal" />
+        </CardContent>
+        
+        <CardActions>
+        <Button fullWidth variant="contained">Login</Button>
+        </CardActions>
+      </Card>
+        </div>
     )
 }
